c3b1b8a9c08c4dd3a558b4ce4a2c0e9a202926f6
yangtao <431910788@qq.com>
2018-07-27 14:36:04 +0800

    maven docker 一体提交

---
+++ maven-docker/src/main/java/com/example/demo/MavenDockerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDockerApplication.class, args);
+	}
+}
